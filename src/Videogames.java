0fb8c68cadc6488e6989d7a9b822aba5380d5f42
sroy8434 <sidhartharoy7@gmail.com>
2019-02-06 14:49:49 -0500

    yurt gogurt

---
+++ src/Videogames.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Videogames
+{
+    private ArrayList<Videogame> collection;
+
+    public Videogames()
+    {
+        collection = new ArrayList<Videogame>();
+    }
+}
