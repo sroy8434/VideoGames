279b92a07174fd3683d9e6c83fa4ba3b95bc7f1b
sroy8434 <sidhartharoy7@gmail.com>
2019-02-06 14:35:18 -0500

    yurt gogurt

---
+++ src/Videogame.java
@@ -0,0 +1,40 @@
+public class Videogame
+{
+    //instance variables
+    private String game;
+    private String genre;
+    private String creator;
+    private int year;
+    private boolean singleplayer;
+    private boolean multiplayer;
+    private double gbSize;
+
+   //constructor
+   public Videogame(String game, String genre, String creator, int year, boolean singleplayer, boolean multiplayer, double gbSize)
+   {
+       this.game = game;
+       this.genre = genre;
+       this.creator = creator;
+       this.year = year;
+       this.singleplayer = singleplayer;
+       this.multiplayer = multiplayer;
+       this.gbSize = gbSize;
+   }
+
+   //accessors
+   public String getGame()         {return game;}
+   public String getGenre()        {return genre;}
+   public String getCreator()      {return creator;}
+   public int getYear()            {return year;}
+   public boolean isSingleplayer() {return singleplayer;}
+   public boolean isMultiplayer()  {return multiplayer;}
+   public double getGbSize()       {return gbSize;}
+
+   public String toString()
+   {
+       String output = "";
+
+       if ()
+   }
+
+}
